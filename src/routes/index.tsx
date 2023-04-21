4382903750bf2ff21eb82f622a66f520b3119afa
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-04-21 15:43:42 -0300

    organized routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
+
+// Main Menu
+import { MainMenu } from '../layouts/MainMenu'
+
+// Pages
+import { Home } from '../pages/home'
+import { Crew } from '../pages/crew'
+import { Destination } from '../pages/destination'
+import { Technology } from '../pages/technology'
+import { NotFound } from '../pages/NotFound'
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainMenu />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
